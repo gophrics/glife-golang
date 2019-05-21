36d9202a802da884986de912bbdf9022108f7aa7
Nitin Issac Joy <nitin.i.joy@gmail.com>
2019-05-21 22:20:09 +0530

    Travel info struct defined

--- travel/Service/APIParameterStructures.go
+++ travel/Service/APIParameterStructures.go
@@ -5,5 +5,21 @@ type TravelInfo struct {
 	TravelId  int64 `json:"travelId"`
 }
 
+type Region struct {
+	Latitude       float64 `json:"latitude"`
+	Longitude      float64 `json:"longitude"`
+	LatitudeDelta  float64 `json:"latitudeDelta"`
+	LongitudeDelta float64 `json:"longitudeDelta"`
+}
+
+type ImageData struct {
+	Location  Region `json:"region"`
+	Image     string `json:"image"`
+	Timestamp int64  `json:"timestamp"`
+}
+
 type TravelResponse struct {
+	Region       Region           `json:"region"`
+	ImageData    []ImageData      `json:"imageData"`
+	TimelineData map[int][]string `json:"timelinedata"`
 }
