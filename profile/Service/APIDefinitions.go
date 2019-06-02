67cfc7d80c1c5ffbf9827fea4101a27e6076001a
Nitin Issac Joy <nitin.i.joy@gmail.com>
2019-06-02 21:04:24 +0530

    Google OAuth registration API

--- profile/Service/APIDefinitions.go
+++ profile/Service/APIDefinitions.go
@@ -19,6 +19,8 @@ import (
 func Routes() *chi.Mux {
 	router := chi.NewRouter()
 	router.Post("/api/v1/profile/register", RegisterUser)
+	router.Post("/api/v1/profile/registerWithGoogle", RegisterUserWithGoogle)
+	router.Post("/api/v1/profile/loginWithGoogle", LoginUserWithGoogle)
 	router.Get("/api/v1/profile/search/{searchstring}", FindUser)
 	router.Get("/api/v1/profile/getuser/{profileId}", GetUser)
 	return router
@@ -26,6 +28,79 @@ func Routes() *chi.Mux {
 
 func NewProfileId() primitive.ObjectID {
 	return primitive.NewObjectID()
+}
+
+func LoginUserWithGoogle(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func RegisterUserWithGoogle(w http.ResponseWriter, r *http.Request) {
+	b, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	var req RegisterUserWithGoogleRequest
+
+	json.Unmarshal(b, &req)
+
+	// Verify google auth token is valid for our client
+	resp, err := http.Get(fmt.Sprintf("https://oauth2.googleapis.com/tokeninfo?id_token=%s", req.Token))
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	fmt.Printf("1")
+	var resp2 GoogleAuthVerification
+	json.Unmarshal(body, &resp2)
+
+	fmt.Printf("%s", resp2.Aud)
+	if resp2.Aud != "249369235819-11cfia1ht584n1kmk6gh6kbba8ab429u.apps.googleusercontent.com" {
+		http.Error(w, err.Error(), 500)
+	}
+
+	fmt.Printf("3\n", resp2)
+	// Check if user exist in database
+	filter := bson.D{
+		{"email", resp2.Email},
+	}
+
+	var profileInDB GetUserResponse
+	err = mongodb.Profile.FindOne(context.TODO(), filter).Decode(&profileInDB)
+
+	fmt.Printf("4\n", resp2)
+	if err == nil {
+		http.Error(w, "Profile already exist", 500)
+	}
+
+	var result RegisterUserResponse
+	result.Country = ""
+	result.Email = resp2.Email
+	result.Name = resp2.Name
+	result.Phone = ""
+	result.ProfileId = primitive.NewObjectID()
+
+	insertResult, err := mongodb.Profile.InsertOne(context.TODO(), result)
+
+	fmt.Printf("5\n", resp2)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+	}
+
+	fmt.Println("Inserted document from google auth: ", insertResult)
+
+	response := make(map[string]string)
+	response["OperationStatus"] = "Success"
+	response["Result"] = fmt.Sprintf("%s", result)
+	render.JSON(w, r, response)
 }
 
 func RegisterUser(w http.ResponseWriter, r *http.Request) {
@@ -46,7 +121,7 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 	result.Email = req.Email
 	result.Name = req.Name
 	result.Phone = req.Phone
-	result.ProfileId = NewProfileId()
+	result.ProfileId = primitive.NewObjectID()
 
 	insertResult, err := mongodb.Profile.InsertOne(context.TODO(), result)
 	if err != nil {
--- profile/Service/APIParameterStructures.go
+++ profile/Service/APIParameterStructures.go
@@ -25,10 +25,30 @@ type RegisterUserRequest struct {
 	Country string `json:"country"`
 }
 
+type RegisterUserWithGoogleRequest struct {
+	Token string `json:"token"`
+}
+
 type RegisterUserResponse struct {
 	ProfileId primitive.ObjectID `json:"id" bson:"_id"`
 	Name      string             `json:"name"`
 	Email     string             `json:"email"`
 	Phone     string             `json:"phone"`
 	Country   string             `json:"country"`
+}
+
+type GoogleAuthVerification struct {
+	Iss           string `json:"iss"`
+	Sub           string `json:"sub"`
+	Azp           string `json:"azp"`
+	Aud           string `json:"aud"`
+	Iat           string `json:"iap"`
+	Exp           string `json:"exp"`
+	Email         string `json:"email"`
+	EmailVerified string `json:"email_verified"`
+	Name          string `json:"name"`
+	Picture       string `json:"picture"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
+	Locale        string `json:"locale"`
 }
